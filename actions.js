2d2950f345e20449183cae28915946e0ece4bb14
Kamil Szczypior <kszczypior1@gmail.com>
2018-10-30 22:32:10 +0100

    Mod action.js

--- actions.js
+++ actions.js
@@ -1,11 +1,13 @@
 import uuid from uuid;
 
-export const ADD_COMMENT = 'ADD_COMMENT' //Tworzenie komentarza
+export const ADD_COMMENT = 'ADD_COMMENT'; //Tworzenie komentarza
 export const REMOVE_COMMENT = 'REMOVE_COMMENT'; //usuwanie komentarza
 export const EDIT_COMMENT = 'EDIT_COMMENT'; // edycja komentarza
 export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT'; //like
 export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT'; //unlike
 
+
+// Kreatory akcji
 function addComment(text) {
     return {
         type: ADD_COMMENT,
